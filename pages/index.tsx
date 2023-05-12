18cb9022ccd4d6d22d71cad4e822b2b791d20024
Azul Heynen <azulheynen@gmail.com>
2023-05-12 03:30:03 -0300

    Images with masonary layput

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
 import { Tab } from "@headlessui/react";
+import Masonry from "react-masonry-css"
 
 
 
@@ -22,13 +23,13 @@ const tabs = [
 
 export default function Home() {
   return (
-    <div className=" flex flex-col h-full bg-[url('/fotodos.avif')] bg-cover ">
+    <div className=" h-full bg-[url('/fotodos.avif')] bg-cover overflow-auto">
       <Head>
         <title>my portfolio </title>
         <meta></meta>
         <link></link>
       </Head>
-      <header className="flex justify-between items-center h-[70px] px-6">
+      <header className="bg-stone-900 fixed top-0 w-full z-10 flex justify-between items-center h-[70px] px-6">
        
         <div >Portfolio</div>
         <Link
@@ -39,7 +40,7 @@ export default function Home() {
         </Link>
       </header>
 
-      <main className="flex-grow ">
+      <main className="pt-[110px]">
         <div className="flex flex-col items-center h-full">
           <Tab.Group >
             <Tab.List className="flex items center gap-24">
@@ -55,8 +56,19 @@ export default function Home() {
 
             ))}
             </Tab.List>
-            <Tab.Panels  className=" h-full bg-stone-600 bg-opacity-20 max-w-[900px] w-full p-2 sm:p-4 my-6">
-              <Tab.Panel >All Images</Tab.Panel>
+            <Tab.Panels  className=" h-full  max-w-[900px] w-full p-2 sm:p-4 my-6">
+              <Tab.Panel >
+                <Masonry breakpointCols={2}  className="flex gap-5" columnClassName="">
+                  <img src="/platiuno.jpg" alt="plati-1" className="my-4" />
+                  <img src="/platidos.png" alt="plati-2"  className="my-4"/>
+                  <img src="/platitres.png" alt="plati-3"  className="my-4"/>
+                  <img src="/platicuatro.png" alt="plati-4" className="my-4" />
+                  <img src="/honguito.png" alt="honguito" className="my-4" />
+                  <img src="/honguitouno.png" alt="honguito-uno" className="my-4" />
+                  <img src="/honguitodos.png" alt="honguito-dos" className="my-4" />
+                
+                </Masonry>
+              </Tab.Panel>
               <Tab.Panel>Digital</Tab.Panel>
               <Tab.Panel>More </Tab.Panel>
             </Tab.Panels>
